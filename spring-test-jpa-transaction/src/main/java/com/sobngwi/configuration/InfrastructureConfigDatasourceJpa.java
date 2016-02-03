b77147aabca5b8fea10bbbaaf76fe0498829911b
Alain Narcisse SOBNGWI <sobngwi@gmail.com>
2016-02-03 03:43:44 +0100

    Segregation of Datasource.

--- spring-test-jpa-transaction/src/main/java/com/sobngwi/configuration/InfrastructureConfigDatasourceJpa.java
+++ spring-test-jpa-transaction/src/main/java/com/sobngwi/configuration/InfrastructureConfigDatasourceJpa.java
@@ -1,6 +1,5 @@
 package com.sobngwi.configuration;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -11,55 +10,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.sobngwi.repository.AccountRowMapper;
-
-@Configuration 
+@Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:prod.properties")
-@ComponentScan({"com.sobngwi.repositoryjdbc.impl"})
-public class InfrastructureConfigJdbc {
+@ComponentScan({"com.sobngwi.repositoryjpa.impl"})
+public class InfrastructureConfigDatasourceJpa {
 
 	@Autowired
 	private Environment env;
-	@Resource
-	DataSource dataSource ;
-	
-	@Bean ( name = "transactionManager")
-	public PlatformTransactionManager transactionManagerForTest(){
-		return new DataSourceTransactionManager(datasourceForTest());
-	}
-	
-	@Bean ( name = "transactionManager")
-	public PlatformTransactionManager transactionManagerForProd(){
-		return new DataSourceTransactionManager(datasourceForprod());
-	}
-
-	@Bean
-	public AccountRowMapper accountRowMapper(){
-		return new AccountRowMapper();
-	}
 	
 	@Bean ( name = "dataSource")
-	@Profile("jdbc-test")
-	public DataSource datasourceForTest(){
-		return new EmbeddedDatabaseBuilder()
-				   .generateUniqueName(true)
-				   .setType(EmbeddedDatabaseType.H2)
-				   .setScriptEncoding("UTF-8")
-				   .addScript("schema.sql")
-				   .addScripts("data.sql")
-				   .build();
-	}
-	
-	@Bean ( name = "dataSource")
-	@Profile("jdbc-prod")
-	public DataSource datasourceForprod(){
+	@Profile("jpa-prod")
+	public DataSource dataSourceProdJpa(){
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName(env.getProperty("db.driver"));
 		dataSource.setUrl(env.getProperty("db.url"));
@@ -68,6 +34,18 @@ public class InfrastructureConfigJdbc {
 		
 		return dataSource;
 	}
+	@Bean ( name = "dataSource")
+	@Profile("jpa-test")
+	public DataSource dataSourceTestJpa(){
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(env.getProperty("db.driver"));
+		dataSource.setUrl(env.getProperty("db.url"));
+		dataSource.setUsername(env.getProperty("db.user"));
+		dataSource.setPassword(env.getProperty("db.pass"));
+		
+		return dataSource;
+	}
+	
 	
 
 }
